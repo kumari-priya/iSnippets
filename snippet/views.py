bfa5b21a359cfe104bf3901f585d09b423c68b79
Kumari Priya <priya@goel.pw>
2017-12-21 22:20:02 -0800

    static files

--- snippet/views.py
+++ snippet/views.py
@@ -9,7 +9,6 @@ def index(request):
     if request.method == 'POST':
         form = SnippetForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             title = form.cleaned_data['title']
             language = form.cleaned_data['language']
             code_snippet = form.cleaned_data['code_snippet']
